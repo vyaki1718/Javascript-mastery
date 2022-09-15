f82ff8b222db71a16c7528c6ba0881df45afbc76
Vivek Patil <vivekpatil1718@gmail.com>
2022-09-15 11:20:47 +0530

    shallow and deep cpoy doen

--- object_copy.js
+++ object_copy.js
@@ -7,14 +7,43 @@
 
 // console.log(obj);
 
+//shallow copy 
+//shallow copy does not work with nested object
+// let obj = {
+//     name: "peter"
+// }
 
+// // let user = Object.assign({}, obj);
+// let user = { ...obj } // by destructuring
+// user.name = "anil";
+
+// console.log("object is : ", obj);
+// console.log("user is : ", user);
+
+
+
+//deep copy
+// deep copy does not work if object have function
 let obj = {
-    name: "peter"
+    name: "peter",
+    address: {
+        city: "pune",
+        state: "MH"
+    },
+    getData: function ()
+    {
+
+    }
+
 }
 
-// let user = Object.assign({}, obj);
-let user = { ...obj } // by destructuring
-user.name = "anil";
+// let user = JSON.parse(JSON.stringify(obj));
+// user.address.city = "amalner";
+
+//to solve deep copy limitation we have to lodash library
+
+let user = _.cloneDeep(obj);
+user.address.city = "amalner";
 
 console.log("object is : ", obj);
 console.log("user is : ", user);
