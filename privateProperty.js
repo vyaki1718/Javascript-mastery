5ec2d0f6e42be942333d2150b481b78a2358764f
Vivek Patil <vivekpatil1718@gmail.com>
2022-09-15 14:41:58 +0530

    private property method done

---
+++ privateProperty.js
@@ -0,0 +1,27 @@
+
+//declare class
+
+class BankAccount
+{
+    name = "vivek patil";
+    #pin = "12345";  // #pin  declaring private property
+    getData()
+    {
+        return this.name;
+    }
+    #getifsc()         //#getPin(){}  declare private method
+    {
+        return "0008877";
+    }
+    getDetails()
+    {
+        return {
+            pin: this.#pin,
+            name: this.name,
+            ifsc: this.#getifsc()
+        }
+    }
+}
+
+const details = new BankAccount();
+console.log(details.getDetails())
